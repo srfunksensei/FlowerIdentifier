c6c1e3c925d6f3881691aa3dc913303f890a2e13
srfunksensei <sr.funk.sensei@gmail.com>
2020-10-01 09:57:50 +0200

    added logic for flower identification on image selection

--- FlowerIdentifier/ContentView.swift
+++ FlowerIdentifier/ContentView.swift
@@ -7,9 +7,11 @@
 //
 
 import SwiftUI
+import Vision
 
 struct ContentView: View {
     
+    @State private var title: String = "Identify Flower"
     @State private var isSheetShown = false
     @State private var isShowingImagePicker = false
     @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
@@ -42,10 +44,37 @@ struct ContentView: View {
                         ])
                 }
             }
-            .navigationBarTitle("Identify Flower")
+            .navigationBarTitle(title)
         }
         .sheet(isPresented: $isShowingImagePicker) {
             ImagePickerView(isShown: self.$isShowingImagePicker, selectedImage: self.$image, sourceType: self.sourceType)
+                .onDisappear { self.detectFlower(from: self.image) }
+        }
+    }
+    
+    fileprivate func detectFlower(from image: UIImage) {
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("Cannot convert to ciImage")
+        }
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Cannot import model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results?.first as? VNClassificationObservation else {
+                fatalError("Could not complete classfication")
+            }
+            
+            self.title = result.identifier.capitalized
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: ciImage)
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
         }
     }
 }
