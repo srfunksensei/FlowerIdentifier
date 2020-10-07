f92c722f2449613723637b7bca8badcd09b9fe8f
srfunksensei <sr.funk.sensei@gmail.com>
2020-10-07 14:15:28 +0200

    updated UI to display description text upon classification

--- FlowerIdentifier/ContentView.swift
+++ FlowerIdentifier/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     fileprivate let wikipediaURl = "https://en.wikipedia.org/w/api.php"
     
     @State private var title: String = "Identify Flower"
+    @State private var description: String = ""
     @State private var isSheetShown = false
     @State private var isShowingImagePicker = false
     @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
@@ -23,16 +24,34 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Image(uiImage: image)
-                    .resizable()
-                    .frame(width: 300, height: 300, alignment: .center)
+                ScrollView(showsIndicators: false) {
+                    Image(uiImage: image)
+                        .resizable()
+                        .frame(width: 300, height: 300, alignment: .center)
+                    
+                    Text(description)
+                        .font(.body)
+                        .padding()
+                }
                 
-                Button("Choose picture") {
-                    self.isSheetShown = true
-                }.padding()
+                ZStack(alignment: .bottom) {
+                    Button(action: {
+                        self.isSheetShown = true
+                    }) {
+                        HStack {
+                            Image(systemName: "photo")
+                            Text("Choose picture")
+                                .font(.callout)
+                                .fontWeight(.medium)
+                        }
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding()
                     .background(Color.green)
                     .foregroundColor(Color.white)
-                    .cornerRadius(10)
+                    .cornerRadius(40)
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 20)
                     .actionSheet(isPresented: $isSheetShown) {
                         ActionSheet(title: Text("Select photo"), message: nil, buttons: [
                             .default(Text("Photo library")) {
@@ -45,6 +64,7 @@ struct ContentView: View {
                             },
                             .cancel()
                         ])
+                    }
                 }
             }
             .navigationBarTitle(title)
